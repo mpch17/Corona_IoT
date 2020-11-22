1ba80360a6bef9888d3114244ea5f616dce2ba6a
Martin Pekár <32843490+MrPekar98@users.noreply.github.com>
2020-11-22 16:09:50 +0100

    Update frame_io_write.cpp

--- server/IO/frame_io_write.cpp
+++ server/IO/frame_io_write.cpp
@@ -3,14 +3,32 @@
 namespace corona
 {
     // Constructor.
-    frame_io_write::frame_io_write(const std::ostream* outstream)
-        : output(outstream)
+    frame_io_write::frame_io_write(const std::ostream* outstream, const std::string& filename)
+        : output(outstream), frame_io(filename)
     {
 
     }
 
     // Destructor.
     frame_io_write::~frame_io_write()
+    {
+
+    }
+
+    // Opens stream.
+    void frame_io_write::open()
+    {
+
+    }
+
+    // Closes stream.
+    void frame_io_write::close()
+    {
+
+    }
+
+    // Writes frame to stream.
+    void frame_io_write::write(const frame& f) const
     {
 
     }
