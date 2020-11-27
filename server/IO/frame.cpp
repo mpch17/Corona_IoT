71656252d6369e89993657f12e292e724dac0e0f
MrPekar98 <martin.pekar@outlook.dk>
2020-11-27 18:27:48 +0100

    Disables exceptions when needed.

--- server/IO/frame.cpp
+++ server/IO/frame.cpp
@@ -1,11 +1,12 @@
 #include "frame.hpp"
 #include <stdexcept>
-#include "../config.hpp"
 
 namespace corona
 {
+    static node invalid_node = invalid_node_t();
+
     // Adds node to graph.
-    void frame::add_node(const node& n) throw()
+    void frame::add_node(const node& n) HAS_EXCEPT
     {
         check_structure();
 
@@ -15,11 +16,17 @@ namespace corona
         else if (n.is_edge())
         {
             if (n.get_edge1() >= this->graph.size() || n.get_edge2() >= this->graph.size())
+#if NO_EXCEPT
+                return;
+#else
                 throw std::invalid_argument("Edges do not exist in graph.");
+#endif
 
             node n1 = findby_id(n.get_edge1());
             node n2 = findby_id(n.get_edge2());
-            this->graph[n1.get_index()][n2.get_index()] = HALLWAY_CAPACITY - n.get_people_count();
+
+            if (!(n1 == invalid_node_t()) && !(n2 == invalid_node_t()))
+                this->graph[n1.get_index()][n2.get_index()] = HALLWAY_CAPACITY - n.get_people_count();
         }
 
         else
@@ -35,7 +42,7 @@ namespace corona
 
     // Finds node given predicate.
     template<typename predicate>
-    node& frame::find(predicate pred) throw()
+    node& frame::find(predicate pred) HAS_EXCEPT
     {
         for (node& n : this->nodes)
         {
@@ -43,7 +50,12 @@ namespace corona
                 return n;
         }
 
+#if NO_EXCEPT
+        invalid_node = invalid_node_t();
+        return invalid_node;
+#else
         throw std::invalid_argument("No node found.");
+#endif
     }
 
     // Iterates over all row in graph.
@@ -57,9 +69,14 @@ namespace corona
     }
 
     // Converts frame into string representation.
-    std::string frame::to_string() const noexcept
+    std::string frame::to_string() const HAS_EXCEPT
     {
-        check_structure();
+        if (check_structure())
+#if NO_EXCEPT
+            return "";
+#else
+            throw std::runtime_error("Structure of frame is corrupt.");
+#endif
 
         std::string str = "";
 
@@ -76,14 +93,18 @@ namespace corona
     }
 
     // Creates an edge between two nodes found by their longitude and latitude.
-    void frame::create_edge(const node& n1, const node& n2, unsigned short people_count) throw()
+    void frame::create_edge(const node& n1, const node& n2, unsigned short people_count) HAS_EXCEPT
     {
         check_structure();
 
         short n1_idx = find_node(n1), n2_idx = find_node(n2);
 
         if (n1_idx < 0 || n2_idx < 0)
+#if NO_EXCEPT
+            return;
+#else
             throw std::invalid_argument("No nodes with longitude and latitude found.");
+#endif
 
         this->graph[n1_idx][n2_idx] = people_count;
     }
@@ -101,21 +122,31 @@ namespace corona
     }
 
     // Returns true if flow between the two nodes is greater than 0.
-    bool frame::has_edge(const node& n1, const node& n2) const throw()
+    bool frame::has_edge(const node& n1, const node& n2) const HAS_EXCEPT
     {
         short n1_idx = find_node(n1), n2_idx = find_node(n2);
 
         if (n1_idx < 0 || n2_idx < 0)
+#if NO_EXCEPT
+            return false;
+#else
             throw std::invalid_argument("No nodes with longitude and latitude found.");
+#endif
 
         return this->graph[n1_idx][n2_idx] > 0;
     }
 
+    static std::vector<unsigned short> empty_vector;
+
     // Accesses a row in adjacency matrix (graph).
-    const std::vector<unsigned short>& frame::operator[](unsigned i) const throw()
+    const std::vector<unsigned short>& frame::operator[](unsigned i) const HAS_EXCEPT
     {
         if (i >= this->graph.size())
+#if NO_EXCEPT
+            return (empty_vector = std::vector<unsigned short>());
+#else
             throw std::invalid_argument("Index out of bound for graph.");
+#endif
 
         return this->graph[i];
     }
@@ -123,19 +154,24 @@ namespace corona
     // Adds row to graph matrix (adjacency matrix).
     void frame::add_row(const std::vector<unsigned short>& row)
     {
+#if !NO_EXCEPT
         throw std::runtime_error("Operation not supported.");
+#endif
     }
 
     // Adds column to graph matrix (adjacency matrix).
     void frame::add_column(const std::vector<unsigned short>& column)
     {
+#if !NO_EXCEPT
         throw std::runtime_error("Operation not supported.");
+#endif
     }
 
     // Removes row from graph (adjacency matrix).
     void frame::delete_row(unsigned index)
     {
-        this->graph.erase(this->graph.begin() + index);
+        if (index < this->graph.size())
+            this->graph.erase(this->graph.begin() + index);
     }
 
     // Removes column from graph (adjacency matrix).
@@ -154,7 +190,7 @@ namespace corona
 
     // Removes node from graph.
     // Handled differently if edge.
-    void frame::remove_node(const node& n)
+    void frame::remove_node(const node& n) HAS_EXCEPT
     {
         check_structure();
 
@@ -162,13 +198,19 @@ namespace corona
         {
             node n1 = find([&n](const node& n_arg){ return n.get_edge1() == n.get_id(); });
             node n2 = find([&n](const node& n_arg){ return n.get_edge2() == n.get_id(); });
-            this->graph[n1.get_index()][n2.get_index()] = 0;
+
+            if (!(n1 == invalid_node_t()) && !(n2 == invalid_node_t()))
+                this->graph[n1.get_index()][n2.get_index()] = 0;
         }
 
         else
         {
             if (n.get_index() >= this->nodes.size())
+#if NO_EXCEPT
+                return;
+#else
                 throw std::invalid_argument("Cannot remove node: Index out of bounds.");
+#endif
 
             delete_row(n.get_index());
             delete_column(n.get_index());
@@ -188,13 +230,15 @@ namespace corona
 
     // Makes sure graph is square and has the same number of rows/columns as entries in nodes.
     // If something is wrong, an std::runtime_error is thrown.
-    void frame::check_structure() const throw()
+    bool frame::check_structure() const noexcept
     {
         if (this->graph.size() > 0 && this->graph.size() != this->graph[0].size())
-            throw std::runtime_error("Graph matrix is not square: Maybe the number of calls to delete_row and delete_column does not match.");
+            return false;
 
         else if (this->graph.size() != this->nodes.size())
-            throw std::runtime_error("The number of stored nodes does not match the number of nodes in graph.");
+            return false;
+
+        return true;
     }
 
     // Checks whether node (vertex or edge) exists.
@@ -207,12 +251,22 @@ namespace corona
                 node n1 = findby_id(n.get_edge1());
                 node n2 = findby_id(n.get_edge2());
 
+#if NO_EXCEPT
+                if (n1 == invalid_node_t() || n2 == invalid_node_t())
+                    return false;
+#endif
+
                 return this->graph[n1.get_index()][n2.get_index()] > 0;
             }
 
             else
             {
                 node found = findby_id(n.get_id());
+
+#if NO_EXCEPT
+                return !(found == invalid_node_t());
+#endif
+
                 return true;
             }
         }
@@ -224,7 +278,7 @@ namespace corona
     }
 
     // Finds node by ID.
-    inline node& frame::findby_id(const unsigned long& id) throw()
+    inline node& frame::findby_id(const unsigned long& id) HAS_EXCEPT
     {
         return find([&id](const node& n){ return n.get_id() == id; });
     }
--- server/IO/frame.hpp
+++ server/IO/frame.hpp
@@ -3,7 +3,14 @@
 
 #include "matrix.hpp"
 #include "node.hpp"
+#include "../config.hpp"
 #include <string>
+
+#if NO_EXCEPT
+#define HAS_EXCEPT noexcept
+#else
+#define HAS_EXCEPT throw()
+#endif
 
 namespace corona
 {
@@ -19,23 +26,23 @@ namespace corona
         void for_each_row(modifier func) noexcept;
         short find_node(const node& n) const noexcept;
         template<typename predicate>
-        node& find(predicate pred) throw();
-        node& findby_id(const unsigned long& id) throw();
+        node& find(predicate pred) HAS_EXCEPT;
+        node& findby_id(const unsigned long& id) HAS_EXCEPT;
         void decrement_node_indices(unsigned start_index) noexcept;
-        void check_structure() const throw();
+        bool check_structure() const HAS_EXCEPT;
 
     public:
-        void add_node(const node& n) throw();
+        void add_node(const node& n) HAS_EXCEPT;
         std::string to_string() const noexcept;
-        void create_edge(const node& n1, const node& n2, unsigned short people_count) throw();
-        const std::vector<unsigned short>& operator[](unsigned i) const throw() override;
+        void create_edge(const node& n1, const node& n2, unsigned short people_count) HAS_EXCEPT;
+        const std::vector<unsigned short>& operator[](unsigned i) const HAS_EXCEPT override;
         void add_row(const std::vector<unsigned short>& row) override;
         void add_column(const std::vector<unsigned short>& column) override;
         void delete_row(unsigned index) override;
         void delete_column(unsigned index) override;
-        bool has_edge(const node& n1, const node& n2) const throw();
+        bool has_edge(const node& n1, const node& n2) const HAS_EXCEPT;
         const std::vector<node>& get_nodes() const noexcept;
-        void remove_node(const node& n);
+        void remove_node(const node& n) HAS_EXCEPT;
         bool has_node(const node& n) noexcept;
     };
 }
